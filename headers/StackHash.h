2c3c956dbc219f331a1923a5f828fa65d7d7d885
Iprime111 <maximatimoshkin@outlook.com>
2023-09-23 18:44:30 +0300

    hash protection libs

---
+++ headers/StackHash.h
@@ -0,0 +1,14 @@
+#include "Stack.h"
+
+#ifndef STACK_HASH_H_
+#define STACK_HASH_H_
+
+typedef unsigned long long hash_t;
+
+StackErrorCode CompareStackHash (Stack *stk);
+StackErrorCode CompareDataHash (Stack *stk, size_t dataLength);
+
+StackErrorCode ComputeStackHash (Stack *stack, hash_t *hash);
+StackErrorCode ComputeDataHash (Stack *stack, size_t dataLength, hash_t *hash);
+
+#endif
